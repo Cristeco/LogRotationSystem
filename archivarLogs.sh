86852db760a08345f58ceb34961c9b7058921829
ErickDenis <39801599+ErickDenis@users.noreply.github.com>
2024-07-22 21:40:50 -0400

    Update archivarLogs.sh

---
+++ archivarLogs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+LIST_FILES=(
+        "/home/erick/Escritorio/Modulo diplomado/clase 22-07-2024/logs/Apache_2k.log"
+)
+
+ARCHIVE_DIR="/home/erick/Escritorio/Modulo diplomado/clase 22-07-2024/LogsFile"
+mkdir -p "$ARCHIVE_DIR"
+
+DATE=$(date +"%Y%m%d%H%M%S")
+YEAR=$(date +"%Y")
+MONTH=$(date +"%m")
+
+for FILE in "${LIST_FILES[@]}"; do
+        FILE_NAME=$(basename "$FILE" .log)
+
+        mkdir -p "$ARCHIVE_DIR/$FILE_NAME"
+        mkdir -p "$ARCHIVE_DIR/$FILE_NAME/$YEAR"
+        mkdir -p "$ARCHIVE_DIR/$FILE_NAME/$YEAR/$MONTH"
+
+        NEW_FILE="$ARCHIVE_DIR/$FILE_NAME/$YEAR/$MONTH/${FILE_NAME}_logs_$DATE.tar.gz"
+        echo $NEW_FILE
+        tar -czf "$NEW_FILE" -C "$(dirname "$FILE")" "$(basename "$FILE")"
+done
